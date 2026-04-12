d298cd5568f069a4caecc00a1a3b463ee40e6133
Nate <natemcc30@gmail.com>
2026-04-12 12:28:18 -0700

    updated app.jsx ping

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Contact from './components/Contact'
 
 function App() {
   useEffect(() => {
-    fetch('https://portfolio-m9jm.onrender.com')
+    fetch('https://portfolio-m9jm.onrender.com/api/ping')
   }, [])
 
   return (
